41821e34e3b772f4c0e6909bc2646cf8e2db9669
Raweeroj <teezap_999@hotmail.com>
2018-09-17 10:42:23 +0700

    chap3

--- src/App.js
+++ src/App.js
@@ -3,19 +3,17 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+  a = "{''world''}"
+  b = "{"
+  c = ""
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div className = "Number">
+        Hello {this.a} " ''
       </div>
     );
   }
 }
+
 
 export default App;
